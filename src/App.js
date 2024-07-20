b0b245792da660ef7dbe285f73599e7bbb4d74e7
Ayush3205 <ayush3205@gmail.com>
2024-07-20 23:52:30 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Restaurant from "./component/Basic/Restaurant";
+import "./component/Basic/style.css";
+import "./component/Basic/menuApi";
+
+function App() {
+  return (
+   <Restaurant/>
+  );
+}
+
+export default App;
